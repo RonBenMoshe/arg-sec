f48f075ece21d2b90eb0a31dcb3d77ea85bdd9b8
Ron Ben Moshe <ron.ben-moshe@checkmarx.com>
2024-02-09 00:03:12 +0200

    Change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 import os
 def main():
     os.umask(0)
-    f = open("/tmp/test.txt", "w",mode=0o777)
+    f = open("/tmp/test.txt","w+")
     f.write("Current Date and time is: {}\n".format(datetime.datetime.now()))
     f.write("OS name and version is: {}".format(platform.platform(aliased=True)))
     f.close()
